e41a50f3f88560e4bf54e8160665cb7885ec7127
ekitale <munjueko@gmail.com>
2024-08-02 12:05:48 +0300

    Add files via upload

--- bmw/conversion.js
+++ bmw/conversion.js
@@ -70,7 +70,7 @@ const alea = (ext) => {
     }
 
     sticker = new Sticker(buffer, {
-      pack:"BMW-Md" ,
+      pack:"Beltah-Md" ,
       author: nomAuteurMessage,
       type:
         arg.includes("crop") || arg.includes("c")
