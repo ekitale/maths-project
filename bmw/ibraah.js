7d499e9e719fb2dd542f07c5de777e29b1892970
ekitale <munjueko@gmail.com>
2024-08-05 18:36:43 +0300

    Update ibraah.js

--- bmw/ibraah.js
+++ bmw/ibraah.js
@@ -1,17 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
-zokou({ nomCom: "ibrahim", reaction: "🧒", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-    console.log("Commande saisie !!!s");
-    let z = '*Yes you called me whats up Ok anyway support me by following my channel and subscribing on my youtube channel* 🙏 \n\n ' + "║❒ 𝙒𝘼𝘾𝙃𝘼𝙉𝙉𝙀𝙇 https://whatsapp.com/channel/0029VaZuGSxEawdxZK9CzM0Y";
-    let d = '                                                     ║❒ 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 https://www.youtube.com/@ibrahimaitech';
-    let varmess = z + d;
-    var mp4 = 'https://telegra.ph/file/dec033e457813cd2ff2e3.mp4';
-    await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
-    //console.log("montest")
-});
-console.log("mon test");
-
 zokou({ nomCom: "bau", reaction: "🚗", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
     let z = '\n\n ' + "";
