fbe81b6734fe9e93df3685e7f4f33aaccb6fc536
ekitale <munjueko@gmail.com>
2024-08-05 18:24:20 +0300

    Update ibraah.js

--- bmw/ibraah.js
+++ bmw/ibraah.js
@@ -12,13 +12,13 @@ zokou({ nomCom: "ibrahim", reaction: "🧒", nomFichier: __filename }, async (de
 });
 console.log("mon test");
 
-zokou({ nomCom: "bmw", reaction: "🚗", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+zokou({ nomCom: "bau", reaction: "🚗", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
     console.log("Commande saisie !!!s");
-    let z = '𝒀𝒆𝒔😉 𝑨𝒎 𝑩𝑴𝑾 𝑴𝑫 𝑨 𝑾𝒉𝒂𝒕𝒔𝒂𝒑𝒑 𝑩𝒐𝒕 𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑩𝒚 𝑰𝒃𝒓𝒂𝒉𝒊𝒎 𝑨𝒅𝒂𝒎𝒔\n\n ' + "𝑰𝒇 𝒚𝒐𝒖 𝒘𝒂𝒏𝒕 𝒕𝒐 𝒖𝒔𝒆 𝒎𝒆 𝒋𝒖𝒔𝒕 𝒕𝒚𝒑𝒆 𝒎𝒆𝒏𝒖 𝒕𝒐 𝒄𝒐𝒎𝒎𝒂𝒏𝒅 𝒎𝒆";
-    let d = '                                                🏁𝑨𝒎 𝒕𝒉𝒆 𝒇𝒂𝒔𝒕𝒆𝒔𝒕 𝒃𝒐𝒕 𝒊𝒏 𝒕𝒉𝒆 𝒘𝒐𝒓𝒍𝒅';
+    let z = '\n\n ' + "";
+    let d = '                                                ';
     let varmess = z + d;
-    var mp4 = 'https://telegra.ph/file/d98532bb4aa4569e92afb.mp4';
-    await zk.sendMessage(dest, { video: { url: mp4 }, caption: varmess });
+    var mp4 = '/bmw.mp4';
+    await zk.sendMessage(dest, { audio: { url: mp3 }, caption: varmess });
     //console.log("montest")
     
 });
