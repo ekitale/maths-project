3452fc8493e3fd501e2f754eac355f5d38586371
ekitale <munjueko@gmail.com>
2024-08-07 00:36:54 +0300

    Update tts.js

--- bmw/tts.js
+++ bmw/tts.js
@@ -58,10 +58,10 @@ const {ms,arg,repondre} = commandeOptions;
       if (!arg[0]) {repondre("Insert a word");return} ;
  const mots = arg.join(" ")
 
-const url = googleTTS.getAudioUrl( nana, {
+const url = googleTTS.getAudioUrl( mots, {
   lang: 'en',
   slow: false,
-  host: 'https://github.com/DGXeon/Tiktokmusic-API/raw/master/tiktokmusic/sound',
+  host: 'https://translate.google.com',
 });
 console.log(url); 
              zk.sendMessage(dest, { audio: { url:url},mimetype:'audio/mp4' }, { quoted: ms,ptt: true });
