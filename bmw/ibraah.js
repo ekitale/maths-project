800b511cb823de5ed10af842f79d314e06296851
ekitale <munjueko@gmail.com>
2024-08-07 20:20:14 +0300

    Update ibraah.js

--- bmw/ibraah.js
+++ bmw/ibraah.js
@@ -1,41 +1,18 @@
 const { zokou } = require("../framework/zokou");
+const ffmpeg = require("fluent-ffmpeg");
 zokou( {
   nomCom : "bao",
  categorie : "general",
   reaction : "😂" },
       async (dest, zk, commandeOptions) => {
+        const fileStream = fs.createWriteStream(filename);
+      audioStream.pipe(fileStream);
+
+      fileStream.on('finish', () => {
     const audiovn = './bmw.mp4';
           zk.sendMessage(dest, { audio: { url: audiovn,mimetype:'audio/mp4'},ptt: true,
         waveform:  [100, 0, 100, 0, 100, 0, 100] });
   }
 ) ;
         
-        //console.log("montest")                               }
-/**
-const fortu = (m.quoted || m); 
-         const quoted = (fortu.mtype == 'buttonsMessage') ? fortu[Object.keys(fortu)[1]] : (fortu.mtype == 'templateMessage') ? fortu.hydratedTemplate[Object.keys(fortu.hydratedTemplate)[1]] : (fortu.mtype == 'product') ? fortu[Object.keys(fortu)[0]] : m.quoted ? m.quoted : m; 
-zk.sendMessage(dest, { audio: { url:url},mimetype:'audio/mp4' }, { quoted: ms,ptt: true });
-
-
         
-const audiovn = "./alive.mp3";
-    const dooc = {
-        audio: {
-          url: audiovn
-        },
-        mimetype: 'audio/mp4',
-        ptt: true,
-        waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Dreaded",
-
-        contextInfo: {
-          mentionedJid: [m.sender],
-          externalAdReply: {
-          title: "Hi human, I am Alive",
-          body: "DREADED BOT",
-          thumbnailUrl: "https://telegra.ph/file/a5147a64a5b91d8cf945c.jpg",
-          sourceUrl: '',
-          mediaType: 1,
-          renderLargerThumbnail: true
-          }}
-**/
