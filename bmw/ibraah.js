0a1f9947bf4a9f5d2ee0f8ea67b692d13e6172b8
ekitale <munjueko@gmail.com>
2024-08-07 18:14:09 +0300

    Update ibraah.js

--- bmw/ibraah.js
+++ bmw/ibraah.js
@@ -7,8 +7,9 @@ zokou({ nomCom: "bao", reaction: "🧒", nomFichier: __filename }, async (dest, 
     let d = '                                                                           𝑯𝒆𝒂𝒍𝒕𝒉 𝒔𝒕𝒂𝒕𝒖𝒔✨';
     let varmess = z + d;
     var audiovn = './bmw.mp4';
-    await zk.sendMessage(dest, { audio: { url: audiovn,mimetype:'audio/mp4'}, ptt: true,
-        waveform:  [100, 0, 100, 0, 100, 0, 100], });
+    await zk.sendMessage(dest, { audio: { url: audiovn,mimetype:'audio/mp4'},{ quoted: ms,ptt: true });
+
+
     //console.log("montest")
 });
 console.log("mon test");
